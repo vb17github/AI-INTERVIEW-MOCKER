103d72aa3c4981e7c099fc78eaf031e8119bfbfd
jindalyashika <152054895+jindalyashika@users.noreply.github.com>
2025-02-09 00:31:21 +0530

    Update Header.jsx

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,25 +1,44 @@
-"use client"
-import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import { usePathname } from 'next/navigation'
-import React, { useEffect } from 'react'
+"use client";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import React, { useEffect } from "react";
 
 export const Header = () => {
-    const path=usePathname()
-    useEffect(()=>{
-        console.log(path);
-    })
+  const path = usePathname();
+
+  useEffect(() => {
+    console.log(path);
+  });
+
   return (
-    <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
-        <Image src={'/logo.svg'} alt='logo' width={160} height={100}  style={{ width: "auto", height: "auto" }}   />
-        <ul className='hidden md:flex gap-6'>
-            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard' && 'text-primary font-bold'}`}>Dashboard</li>
-            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/questions' && 'text-primary font-bold'}`}>Courses</li>
-            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/questions' && 'text-primary font-bold'}`}>Job Recommendation</li>
-            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/upgrade' && 'text-primary font-bold'}`}>Upgrade</li>
-            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/how' && 'text-primary font-bold'}`}>How its works?</li>
-        </ul>
-        <UserButton/>
+    <div className="flex p-4 items-center justify-between bg-secondary shadow-sm">
+      <Image
+        src={'/logo.svg'}
+        alt='logo'
+        width={160}
+        height={100}
+        style={{ width: "auto", height: "auto" }}
+      />
+      <ul className='hidden md:flex gap-6'>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard' && 'text-primary font-bold'}`}>
+          <Link href="/dashboard">Dashboard</Link>
+        </li>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/questions' && 'text-primary font-bold'}`}>
+          <Link href="/dashboard/questions">Courses</Link>
+        </li>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/job-recommendation' && 'text-primary font-bold'}`}>
+          <Link href="/dashboard/job-recommendation">Job Recommendation</Link>
+        </li>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/upgrade' && 'text-primary font-bold'}`}>
+          <Link href="/dashboard/upgrade">Upgrade</Link>
+        </li>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/how' && 'text-primary font-bold'}`}>
+          <Link href="/dashboard/how">How it works?</Link>
+        </li>
+      </ul>
+      <UserButton />
     </div>
-  )
-}
+  );
+};
