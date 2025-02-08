7bca939219a49558260acaad611575f8415e56c4
jindalyashika <152054895+jindalyashika@users.noreply.github.com>
2025-02-08 18:06:41 +0530

    Update Header.jsx

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -15,6 +15,7 @@ export const Header = () => {
         <ul className='hidden md:flex gap-6'>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard' && 'text-primary font-bold'}`}>Dashboard</li>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/questions' && 'text-primary font-bold'}`}>Courses</li>
+            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/questions' && 'text-primary font-bold'}`}>Job Recommendation</li>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/upgrade' && 'text-primary font-bold'}`}>Upgrade</li>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/how' && 'text-primary font-bold'}`}>How its works?</li>
         </ul>
