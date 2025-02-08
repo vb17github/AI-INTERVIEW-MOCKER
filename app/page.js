1888b957a82fc89eb1c36f64a21dc1f93ba6e7f2
Yogita <yogitag185@gmail.com>
2025-02-08 21:51:57 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,10 +3,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div>
-    <h1>hello world</h1>
-    <Button>Subscribe</Button>
+    <div className="min-h-screen bg-gradient-to-b from-black to-gray-900 flex flex-col md:flex-row items-center justify-center">
+      {/* Header Section */}
+      <div className="text-center text-white ">
+        <h1 className="text-6xl font-bold mb-4">AI Mock Interviewer </h1>
+        <p className="text-xl text-gray-400 mb-8 p-6 md:p-12">
+        Ace your next job interview with our AI-powered mock interviews! Get instant feedback, real-time practice, and expert-level insights to boost your confidence and performance.
+        </p>
 
-  </div>
+        <a href="/dashboard" target="_blank"><button className="px-6 py-3 bg-blue-500 hover:bg-blue-600 text-white rounded-full shadow-lg shadow-blue-500/50">
+          Join Us.
+        </button>
+        </a>
+      </div>
+
+      {/* Display Section */}
+      
+    </div>
   );
 }
